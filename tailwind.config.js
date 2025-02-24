88400468633dddaa17689bd9e1bdc5bcd86c1358
InkShadeX <130751290+InkShadeX@users.noreply.github.com>
2025-02-24 20:16:42 +0100

    1.0 (FIX)

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,6 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
     ],
 
     theme: {
@@ -17,8 +16,6 @@ export default {
             },
         },
     },
-
-    darkMode: 'class',  // Esta línea habilita el modo oscuro basado en clases
 
     plugins: [forms],
 };
